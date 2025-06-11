7cf8ddf7e541fda246cb559fc7f2107a09c8efc5
DitteKKoustrup <dks@umbraco.dk>
2025-06-11 10:52:06 +0200

    delete path for content picker

--- umbracoXvanillaJavaScript/src/pages/home.js
+++ umbracoXvanillaJavaScript/src/pages/home.js
@@ -4,7 +4,6 @@ import { TextArea } from '../components/textAreaRender.js';
 import { TextBox } from '../components/textBoxRender.js';
 import { DateTime } from '../components/dateTimeRender.js'
 import { DropDown } from '../components/dropDownRender.js';
-import { ContentPicker } from '../components/contentPickerRender.js';
 import { Numeric } from '../components/numericRender.js';
 import { UrlPicker } from '../components/UrlPickerRender.js';
 import { TinyMce } from '../components/tinyMceRender.js';
@@ -20,7 +19,6 @@ export async function Home() {
     await TextBox("013a704d-d2fb-4798-bea1-bc4f4833bc60");
     await DateTime("b3d07a05-7b68-4bec-91aa-10455526b62c");
     await DropDown("dde9748e-83b9-4059-89ed-0a2798296cc6");
-    await ContentPicker("0e3be28c-c1c0-46c4-96a3-5e7f6ca814ee");
     await Numeric("40fd6e78-d542-4113-8344-05046df31690");
     await UrlPicker("6e2b7854-4ce6-4002-8f46-3435c27c2cc6");
     await TinyMce("3bd1ac80-f490-471f-acf5-535af9333906");
